4f8761890febbccbf15a2028503e7cb426bbe083
nvrmr <102398462+n3vrmr@users.noreply.github.com>
2022-07-06 23:23:16 -0300

    Add files via upload

--- sorteio_vouchers.py
+++ sorteio_vouchers.py
@@ -13,9 +13,11 @@ def importar_arquivo(arquivo):
     """
     Importa um arquivo para ser lido pelo programa. O arquivo deve estar na
     mesma pasta que o programa no computador.
+    
     Argumentos
     ----------
     arquivo : nome do arquivo em formato .xlsx a ser utilizado.
+    
     Returns
     -------
     dataframe : objeto do tipo DataFrame.
@@ -28,8 +30,13 @@ teste = importar_arquivo('Relatório dos pedidos.xlsx')
 
 def limpar_planilha(df):
     """
-    Limpa o dataframe escolhido pelo argumento, tirando todas as linhas que tenham
-    0 como valor na coluna 'Valor Pedido'.
+    Limpa o dataframe escolhido pelo argumento, tirando todas as linhas que não
+    tenham 'Sucesso' na coluna 'Situação'.
+    
+    Argumentos
+    -------
+    df : objeto do tipo DataFrame.
+    
     Returns
     -------
     planilha: objeto do tipo DataFrame apenas com as linhas que serão utilizadas 
@@ -39,29 +46,45 @@ def limpar_planilha(df):
     return planilha
 
 teste = limpar_planilha(teste)
-users = []
-valor = []
-for nome in teste["Usuário"]:
-    tentativa = teste[teste['Usuário'] == nome]
-    total = sum(tentativa["Valor Pedido"])
-    tentativa = tentativa.drop_duplicates("Usuário")
-    users.append(tentativa["Usuário"].values[0])
-    valor.append(total)
-    
-    
 
-# def somar_valores(nome):
-#     for elements in teste["Usuário"]:
-#         if teste[teste["Usuário] == "Usuário"]:
-#             total = sum(teste[])
-#     return teste
+def somar_valores(df):
+    """
+    Determina o valor total em reais das comprasa realizadas por cada pessoa, 
+    retira as linhas com nomes duplicados, depois cria um novo DataFrame apenas
+    com as informações necessárias para a realização do torneio: nome e o valor
+    total.
 
-# teste = somar_valores()
+    Parameters
+    ----------
+    df : objeto do tipo DataFrame.
+
+    Returns
+    -------
+    para_uso : objeto do tipo DataFrame com duas colunas: 'Usuário' e 'Valor
+    Pedido'.
+
+    """
+    users = []
+    valor = []
+    for nome in df["Usuário"]:
+        tentativa = df[df["Usuário"] == nome]
+        total = sum(tentativa["Valor Pedido"])
+        tentativa = tentativa.drop_duplicates("Usuário")
+        if nome not in users:
+            users.append(tentativa["Usuário"].values[0])
+            valor.append(total)
+    para_uso = pd.DataFrame({"Usuário":users,
+                             "Valor Pedido":valor})
+    return para_uso
+    
+teste = somar_valores(teste)
 
 def qtd_vouchers():
     """
     Calcula a quantidade de vouchers que cada pessoa tem baseado no valor gasto
     em reais. Uma pessoa recebe 1 (um) voucher a cada 10 (dez) reais gastos.
+    Não existe pedido mínimo, então o mínimo de vouchers é 1.
+    
     Returns
     -------
     teste : objeto do tipo DataFrame atualizado com uma nova coluna, que contém
@@ -74,16 +97,18 @@ def qtd_vouchers():
     teste['Vouchers'] = vouchers
     return teste
 
-# qtd_vouchers()
+qtd_vouchers()
 
 def multiplicar_vouchers(nome):
     """
     Multiplica cada entrada no DataFrame pelo número de vouchers que a pessoa
     possui. Esta função existe apenas para ser usada dentro de outra função.
+    
     Argumentos
     ----------
     nome : utiliza o nome das pessoas no DataFrame para poder multiplicar as
     entradas.
+    
     Returns
     -------
     df_repeated : objeto do tipo DataFrame com entradas repetidas.
@@ -96,6 +121,7 @@ def concatenar():
     """
     Cria uma lista e adiciona os vários DataFrames criados para cada pessoa no
     DataFrame inicial à lista. Depois, junta os DataFrames dessa lista em um.
+    
     Returns
     -------
     done : objeto do tipo DataFrame contendo todas as entradas de acordo com o
@@ -108,11 +134,12 @@ def concatenar():
         done = pd.concat(h, ignore_index=True)
     return done
 
-# arquivo_final = concatenar()
+arquivo_final = concatenar()
 
 def sorteio():
     """
     Sorteia uma pessoa do arquivo final aleatoriamente.
+    
     Returns
     -------
     sorteado : nome da pessoa que ganhou o sorteio.
@@ -146,11 +173,11 @@ def sorteio():
     print(text)
     return sorteado
 
-# sorteio()
+sorteio()
 
 def main():
     t.sleep(5)
     print("Este foi o fim do sorteio. A JFH agradece a todos que participaram!")
     
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
